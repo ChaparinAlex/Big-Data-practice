ac3f1a304d48eadd0be1ec33b0ac367638e9f106
ChaparinAlex <oleksandr.chaparin@gmail.com>
2017-09-14 15:23:07 +0300

    Unused folder .idea has been removed. Fixed some problems with running Scala program WordCount.

--- Cervical cancer risk research with Spark/src/main/scala/com/epam/big_data/lab/SparkWordCount.scala
+++ Cervical cancer risk research with Spark/src/main/scala/com/epam/big_data/lab/SparkWordCount.scala
@@ -6,17 +6,14 @@ import org.apache.spark.SparkConf
 object SparkWordCount {
   def main(args: Array[String]) {
     // create Spark context with Spark configuration
-    val sc = new SparkContext(new SparkConf().setAppName("Spark Count"))
+    val sc = new SparkContext(new SparkConf().setAppName("Spark Count").setMaster("local"))
 
     // read in text file and split each document into words
-    val tokenized = sc.textFile("src\\main\\resources\\words.txt").flatMap(_.split(" "))
+    val tokenized = sc.textFile("src\\main\\resources\\words.txt").flatMap(_.split(","))
 
     // count the occurrence of each word
     val wordCounts = tokenized.map((_, 1)).reduceByKey(_ + _)
 
-    // count characters
-    val charCounts = wordCounts.flatMap(_._1.toCharArray).map((_, 1)).reduceByKey(_ + _)
-
-    System.out.println(charCounts.collect().mkString(", "))
+    System.out.println(wordCounts.collect().mkString(", "))
   }
 }
