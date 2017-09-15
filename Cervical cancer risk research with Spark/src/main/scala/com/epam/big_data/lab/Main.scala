971565969e4a1da758ce0394908bc54304a60489
ChaparinAlex <oleksandr.chaparin@gmail.com>
2017-09-15 15:26:11 +0300

    Fixed some problems to run the task

--- Cervical cancer risk research with Spark/src/main/scala/com/epam/big_data/lab/Main.scala
+++ Cervical cancer risk research with Spark/src/main/scala/com/epam/big_data/lab/Main.scala
@@ -1,11 +1,11 @@
 package com.epam.big_data.lab
 
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 
 object Main {
 
   def main(args: Array[String]) {
-    val sparkContext = new SparkContext()
+    val sparkContext = new SparkContext(new SparkConf().setAppName("Spark Cancer Risk Research").setMaster("local"))
     val csvOperations = new CSVOperations(sparkContext)
     csvOperations.writeData()
   }
